2719ce5d6229c8acbadcf987775a45068e81ab3d
Neander Wendel <neanderlink123@gmail.com>
2022-08-23 19:04:08 -0300

    refactor: added password in response of mocked login

--- src/modules/auth/api/index.tsx
+++ src/modules/auth/api/index.tsx
@@ -2,6 +2,7 @@ export const login = async (email: string, password: string) => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
     return {
         email,
+        password,
         name: email.split('@')
     };
 }
